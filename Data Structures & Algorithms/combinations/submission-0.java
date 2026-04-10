c6e13570bc7d9b955cc784ed18417f1b23bdc229
Aaditya Mathur <104914201+adityamathur456@users.noreply.github.com>
2026-04-10 21:39:43 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.java
@@ -0,0 +1,24 @@
+
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+            List<List<Integer>> result = new ArrayList<>();
+                    backtrack(1, n, k, new ArrayList<>(), result);
+                            return result;
+                                }
+
+                                    private void backtrack(int start, int n, int k, List<Integer> temp, List<List<Integer>> result) {
+                                            // base case
+                                                    if (temp.size() == k) {
+                                                                result.add(new ArrayList<>(temp));
+                                                                            return;
+                                                                                    }
+
+                                                                                            // generate combinations
+                                                                                                    for (int i = start; i <= n; i++) {
+                                                                                                                temp.add(i);                       // choose
+                                                                                                                            backtrack(i + 1, n, k, temp, result); // explore
+                                                                                                                                        temp.remove(temp.size() - 1);      // un-choose (backtrack)
+                                                                                                                                                }
+                                                                                                                                                    }
+                                                                                                                                                    }
