7d04b06a51915f86a17144b441287fed4b8f3781
Aaditya Mathur <104914201+adityamathur456@users.noreply.github.com>
2026-05-26 21:47:07 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumpCount = 0, currentEnd = 0, farthest = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            farthest = Math.max(farthest, i + nums[i]);
+            if(i == currentEnd){
+                jumpCount++;
+                currentEnd = farthest;
+            }
+        }
+        return jumpCount;
+    }
+}
